bd4ae5362ea42204b5c8e4f7204451a9fa0708fe
Durga-Rajan <31245621+Durga-Rajan@users.noreply.github.com>
2019-10-14 17:45:29 +0530

    Add files via upload

---
+++ SortDirectionSample/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SortDirectionSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ascending.IsChecked = true;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if(radioButton != null && radioButton.IsChecked.Value)
+            {
+                if (radioButton.Content.ToString() == "Ascending")
+                    pgrid.SortDirection = System.ComponentModel.ListSortDirection.Ascending;
+                else if (radioButton.Content.ToString() == "Descending")
+                    pgrid.SortDirection = System.ComponentModel.ListSortDirection.Descending;
+                else
+                    pgrid.SortDirection = null;
+            }
+        }
+    }
+}
---
+++ SortDirectionSample/Person.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using System.ComponentModel.DataAnnotations;
+using System.Windows.Media.Imaging;
+using System.Reflection;
+
+namespace SortDirectionSample
+{
+    public class Person
+    {
+        public Person()
+        {
+            FirstName = "Carl";
+            LastName = "Johnson";
+            Age = 30;
+            Mobile = 91983467382;
+            Email = "carljohnson@gta.com";
+            ID = "0005A";
+            DOB = new DateTime(1987, 10, 16);
+            string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            Image = new BitmapImage(new Uri(path + @"\carl.png", UriKind.RelativeOrAbsolute));
+            Designation = "Team Lead";
+        }
+        
+        [Display(Order = 3)]
+        public Gender Gender
+        {
+            get;
+
+            set;
+        }
+
+        [Display(Order = 7)]
+        public Country Country
+        {
+            get;
+
+            set;
+        }
+       
+
+        [Display(Order = 6)]
+        public string Email
+        {
+            get;
+
+            set;
+        }
+
+        [Display(Order = 0)]
+        public string FirstName
+        {
+            get;
+
+            set;
+        }
+        
+        public string Designation
+        {
+            get;
+
+            set;
+        }
+
+
+        [Display(Order = 1)]
+        public string LastName
+        {
+            get;
+
+            set;
+        }
+
+        public string ID
+        {
+            get;
+
+            set;
+        }
+
+
+        [Display(Order = 4)]
+        public DateTime DOB
+        {
+            get;
+
+            set;
+        }
+
+
+        [Display(Order = 5)]
+        public long Mobile
+        {
+            get;
+
+            set;
+        }
+
+        [Display(Order = 2)]
+        public int Age
+        {
+            get;
+
+            set;
+        }
+
+        public ImageSource Image
+        {
+            get;
+
+            set;
+        }
+    }
+
+    public enum Gender
+    {
+        Male,
+
+        Female
+    }
+
+    public enum Country
+    {
+        UnitedStates,
+
+        Germany,
+
+        Canada,
+
+        Mexico,
+
+        Alaska,
+
+        Japan,
+
+        China,
+
+        Peru,
+
+        Chile,
+
+        Argentina,
+
+        Brazil
+    }
+}
